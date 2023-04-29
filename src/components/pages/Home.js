df4a47f49ae056f74cb90b71dc83459581737b3b
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-04-29 17:39:45 -0500

    layed out tabs

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+       PUT ALL MY STUFF HERE???
+      </p>
+    </div>
+  );
+}
