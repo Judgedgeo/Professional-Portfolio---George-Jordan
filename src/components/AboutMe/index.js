84fd3e0799c19e480489f42fbe00e70daf4f5ccc
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-05-01 21:27:05 -0500

    worked on css, pictures, code for portfolio

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,15 +1,17 @@
 // Recent Photo or Avatar
 // Short Biography
 import React from "react";
+// import 'style.css'
 
 function About() {
   return (
     <div>
       <p className="content is-medium">Full Stack Developer looking to grow and apply the skills I have learned</p>
       <hr />
-      <img className="my-pic" src={process.env.PUBLIC_URL + '../../assets/images/me.jpg'} alt="George K. Jordan"/>
+
+      <img className="me-pic" src={process.env.PUBLIC_URL + '/public/images/me.jpg'} alt="George K. Jordan"/>
       <p className="content is-italic mt-4">
-        I'm George, I am a full stack developer looking to applymy leanrings and grow with a company that focuses ondeveloping people and their skill sets.
+        I'm George, I am a full stack developer looking to apply my leanrings and grow with a company that focuses on developing people and their skill sets.
       </p>
       <p className="content">
         I have 30+ years of management experience, and now look to grow in a field where I can apply my leadership skills as well as what I have learned in full stack developing.
