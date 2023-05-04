7d941419f9542240a9d6f08605766e81d79f96f5
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-05-04 14:34:20 -0500

    wroked on cards for portfolio

--- src/components/Portfolio/Project.js
+++ src/components/Portfolio/Project.js
@@ -1,66 +1,89 @@
-// titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
 
-import React from "react";
 
-function Project(props) {
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import me.jpg from "."
+
+
+function Project() {
   return (
-    <div>
-      <div className="columns is-desktop is-justify-content-center is-flex-wrap-wrap is-flex-direction-row">
-        {props.projects.map((project) => (
-          <div className="column is-half">
-            <div className="card">
-              <div className="card-image">
-                <figure className="image is-4by3">
-                  <a href={project.live} target="_blank" rel="noreferrer">
-                    <img src={process.env.PUBLIC_URL + project.image} alt="Placeholder image" />
-                  </a>
-                </figure>
-              </div>
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-left"></div>
-                  <div className="media-content">
-                    <p className="title is-4" key={project.id}>
-                      {project.title}
-                    </p>
-                  </div>
-                </div>
+    <>
+    <Card style={{ width: '18rem', display: 'inline-block',}}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Less Waste, More Taste</Card.Title>
+        <Card.Text>
+        Recipe App to help you use available ingredients in order to create you a Recipe.
+        </Card.Text>
+        <a href='https://user-images.githubusercontent.com/115055273/236050660-646ba2b4-263f-46b0-b15c-af20335dcd17.png'
+        class = "btn btn-primary">Check it out Here</a>
+        <a href="https://github.com/jdalfaro4/Less_Waste_More_Taste" class = "btn btn-primary">Check out my repo</a>
+      </Card.Body>
+    </Card>
 
-                <div className="content has-text-left">
-                  {project.description}
-                  <br />
-                  <br />
-                  <div className="content is-family-code">
-                    Languages: {project.languages}
-                    <br />
-                    NPM Packages: {project.packages}
-                  </div>
-                  <div className="card">
-                    <footer className="card-footer">
-                      <a
-                        href={project.repo}
-                        className="card-footer-item"
-                        target="_blank" rel="noreferrer"
-                      >
-                        See the Repo!
-                      </a>
-                      <br />
-                      <a
-                        href={project.live}
-                        className="card-footer-item"
-                        target="_blank" rel="noreferrer"
-                      >
-                        See the Live Site!
-                      </a>
-                    </footer>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+     <Card style={{ width: '18rem', display: 'inline-block' }}>
+     <Card.Img variant="top" src="holder.js/100px180" />
+     <Card.Body>
+       <Card.Title>Cutie Pies</Card.Title>
+       <Card.Text>
+       An app that allows you to order your favorite Pies.
+       </Card.Text>
+       <a href='https://user-images.githubusercontent.com/115055273/236050540-162c8515-1c84-4618-be7f-464053b8d184.png'
+       class = "btn btn-primary">Check it out Here</a>
+       <a href='https://github.com/jdalfaro4/Cutie-Pies' class = "btn btn-primary">Check out my repo</a>
+     </Card.Body>
+   </Card>
+
+   <Card style={{ width: '18rem', display: 'inline-block' }}>
+     <Card.Img variant="top" src="holder.js/100px180" />
+     <Card.Body>
+       <Card.Title>The Daily Grind</Card.Title>
+       <Card.Text>
+       A daily Calendar to keep yourself organized.
+       </Card.Text>
+       <a href='https://judgedgeo.github.io/The-Daily-Grind/' class = "btn btn-primary">Check it out Here</a>
+       <a href='https://github.com/Judgedgeo/The-Daily-Grind' class = "btn btn-primary">Check out my repo</a>
+     </Card.Body>
+   </Card>
+
+   <Card style={{ width: '18rem', display: 'inline-block'  }}>
+     <Card.Img variant="top" src="holder.js/100px180" />
+     <Card.Body>
+       <Card.Title>The Weather Detector</Card.Title>
+       <Card.Text>
+       Recipe App to help you use available ingredients in order to create you a Recipe.
+       </Card.Text>
+       <a href='https://judgedgeo.github.io/The-Weather-Detector/' class = "btn btn-primary">Check it out Here</a>
+       <a href='https://github.com/Judgedgeo/The-Weather-Detector' class = "btn btn-primary">Check out my repo</a>
+     </Card.Body>
+   </Card>
+
+   <Card style={{ width: '18rem', display: 'inline-block'  }}>
+     <Card.Img variant="top" src="holder.js/100px180" />
+     <Card.Body>
+       <Card.Title>Power Gen</Card.Title>
+       <Card.Text>
+       Recipe App to help you use available ingredients in order to create you a Recipe.
+       </Card.Text>
+       <a href='https://judgedgeo.github.io/Power-Gen/' class = "btn btn-primary">Check it out Here</a>
+       <a href='https://github.com/Judgedgeo/Power-Gen' class = "btn btn-primary">Check out my repo</a>
+     </Card.Body>
+   </Card>
+
+   <Card style={{ width: '18rem', display: 'inline-block'  }}>
+     <Card.Img variant="top" src="holder.js/100px180" />
+     <Card.Body>
+       <Card.Title>Big Brother Employee Tracker</Card.Title>
+       <Card.Text>
+       Recipe App to help you use available ingredients in order to create you a Recipe.
+       </Card.Text>
+       <a href='https://youtu.be/Fw4cr-QcVJs' class = "btn btn-primary">Check it out Here</a>
+       <a href='https://github.com/Judgedgeo/The-Daily-Grind' class = "btn btn-primary">Check out my repo</a>
+     </Card.Body>
+   </Card>
+</>
   );
 }
 
